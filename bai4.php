7d598efa496c940325a742ac239d601a20d516d3
22070895-cmd <22070895@vnu.edu.vn>
2025-09-23 17:52:02 +0700

    Create bai4.php

---
+++ bai4.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // http://ins3064.test/bai4.php?x=5&y=6
+        $x = $_GET['x'];
+        $y = $_GET['y'];
+        echo "x == y: ". ($x == $y) . "<br/>";
+        echo "x != y: ". ($x != $y) . "<br/>";
+        echo "x < y: ". ($x < $y) . "<br/>";
+        echo "x > y: ". ($x > $y) . "<br/>";
+        echo "x <= y: ". ($x <= $y) . "<br/>";
+        echo "x >= y: ". ($x >= $y) . "<br/>";
+        ?>
+    </body>
+</html>
