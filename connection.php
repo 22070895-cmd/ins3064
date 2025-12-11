b15680f9576688fca2d20ccfd1a0d34960fb88af
22070895-cmd <22070895@vnu.edu.vn>
2025-12-11 23:14:06 +0700

    Update connection.php
    Update database connection information on InfinityFree.

--- connection.php
+++ connection.php
@@ -1,9 +1,12 @@
 <?php
-$conn = new mysqli("localhost", "root", "", "ins3064");
+$host = "sql100.infinityfree.com"; 
+$user = "if0_40633149";      
+$pass = "Phanh20704";  
+$db   = "if0_40633149_final";  
+
+$conn = new mysqli($host, $user, $pass, $db);
 
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    die("Lỗi kết nối database: " . $conn->connect_error);
 }
-
-$conn->set_charset("utf8mb4");
 ?>
